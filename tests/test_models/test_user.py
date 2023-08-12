a8391ebdc9a44d6b43a2d587b1d09a8df837b936
vagrant <vagrant@linux-vag20>
2023-08-12 19:02:01 +0000

    Add unittest for user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""UnitTest for base_model file"""
+import unittest
+from models.user import User
+import models
+
+
+class TestUser_init(unittest.TestCase):
+    """unittest for user class init"""
+    def setUp(self):
+        self.user_obj = User()
+
+    def test_correct_type(self):
+        self.assertEqual(str, type(self.user_obj.email))
+        self.assertEqual(str, type(self.user_obj.password))
+        self.assertEqual(str, type(self.user_obj.first_name))
+        self.assertEqual(str, type(self.user_obj.last_name))
+
+    def test_correct_values(self):
+        self.assertEqual("", self.user_obj.email)
+        self.assertEqual("", self.user_obj.password)
+        self.assertEqual("", self.user_obj.first_name)
+        self.assertEqual("", self.user_obj.last_name)
+
