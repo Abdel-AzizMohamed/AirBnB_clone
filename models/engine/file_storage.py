42f9692758c3951c422a8d78327f680138bad217
vagrant <vagrant@linux-vag20>
2023-08-13 14:01:42 +0000

    Edit filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,27 +13,27 @@ class FileStorage():
 
     def all(self):
         """returns the dictionary __objects class attribute"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """add object to __objects dict"""
         obj_class = obj.__class__.__name__
-        self.__objects.update({"{}.{}".format(obj_class, obj.id): obj})
+        FileStorage.__objects.update({"{}.{}".format(obj_class, obj.id): obj})
 
     def save(self):
         """
             serializes __objects to the json file
             stored in __file_path
         """
-        obj_dict = self.__objects
+        obj_dict = FileStorage.__objects
         con_dict = {obj: obj_dict[obj].to_dict() for obj in obj_dict.keys()}
-        with open(self.__file_path, "w") as w:
+        with open(FileStorage.__file_path, "w") as w:
             json.dump(con_dict, w)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as r:
+            with open(FileStorage.__file_path, "r") as r:
                 json_data = json.load(r)
                 for obj in json_data.values():
                     obj_class = obj["__class__"]
